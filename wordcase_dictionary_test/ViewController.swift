23342c8dbdbdc87941e4d29953da2b536e916d8f
shuhei-cyf <shuhei.cyf@gmail.com>
2019-10-31 10:54:27 +0900

    Initial Commit

---
+++ wordcase_dictionary_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wordcase_dictionary_test
+//
+//  Created by 新谷修平 on 2019/10/31.
+//  Copyright © 2019 Shuhei Shintani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
